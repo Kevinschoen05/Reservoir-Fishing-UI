9555f9608d9e4697331fba910525ed4d7dfdb93a
Kevin Schoen <kevinschoen05@gmail.com>
2022-01-17 16:03:23 -0500

    Added logic to display anglers by total # of fish

--- src/views/Anglers.vue
+++ src/views/Anglers.vue
@@ -10,7 +10,7 @@
       class="search"
     ></v-select>
     <v-row no-gutters class="justify-space-between align-center">
-      <v-col class="pa-3" sm="12" v-for="angler in anglers" :key="angler">
+      <v-col class="pa-3" sm="12" v-for="angler in sortedAnglers" :key="angler">
         <v-card>
           <h1 class="header pa-3">
             {{ angler }}
@@ -36,6 +36,7 @@ export default {
     return {
       records: [],
       anglers: [],
+      sortedAnglers: [],
       years: [
         "All",
         "2022",
@@ -60,7 +61,34 @@ export default {
         var angler = this.records[i].angler;
         anglersList.push(angler);
       }
-      this.anglers = [...new Set(anglersList)];
+      this.orderByTotals(anglersList);
+    },
+
+    //function to sort the anglers into an array by decreasing value
+    orderByTotals(anglersList) {
+      var totals = {};
+      //creates totals object with totals assigned to each angler name as the key
+      for (var name of anglersList) {
+        totals[name] = 0;
+      }
+      for (var i = 0; i < this.records.length; i++) {
+        if (this.records[i].angler in totals) {
+          totals[this.records[i].angler]++;
+        }
+      }
+      //takes the totals object and puts it back in array form
+      var orderedAnglers = [];
+      for (var anglers in totals) {
+        orderedAnglers.push([anglers, totals[anglers]]);
+      }
+      //sorts the array from highest total to lowest
+      orderedAnglers.sort(function (a, b) {
+        return b[1] - a[1];
+      });
+      //pushes the ordered array into the sortedAngers array for use in the v-for
+      for (var k = 0; k < orderedAnglers.length; k++) {
+        this.sortedAnglers.push(orderedAnglers[k][0]);
+      }
     },
   },
   async created() {
