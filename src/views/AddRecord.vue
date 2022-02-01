18c86103f495a130d2e1d7944d9ae63a0a4d6def
Kevin Schoen <kevinschoen05@gmail.com>
2022-01-31 23:10:28 -0500

    work on getting gps location from uploaded pic

--- src/views/AddRecord.vue
+++ src/views/AddRecord.vue
@@ -42,16 +42,36 @@
               v-model="record.comment"
               prepend-icon="mdi-note"
             ></v-textarea>
+            <v-file-input
+              @change="selectFile"
+              show-size
+              counter
+              multiple
+              label="Select Image to Upload Catch Location"
+            >
+            </v-file-input>
             <v-btn type="submit" class="mt-3" color="primary">Add Fish</v-btn>
           </v-form>
         </v-card>
       </v-col>
     </v-row>
+    <script
+      type="application/javascript"
+      src="https://cdn.jsdelivr.net/npm/exif-js"
+    ></script>
   </v-container>
 </template>
 <script>
 import API from "../api";
+import EXIF from "exif-js";
 export default {
+  watch: {
+    image: function () {
+      console.log("Getting coordinate Data");
+      this.coordinates = this.getExif();
+      console.log(this.coordinates);
+    },
+  },
   data() {
     return {
       rules: [(value) => !!value || "This field is required"],
@@ -62,21 +82,87 @@ export default {
         reservoir: "",
         comment: "",
         date: "",
+        image: "",
+        latitude: "",
+        longitude: "",
       },
-      reservoirs:[ "Muscoot", "Croton"],
-      species: ["Large Mouth Bass", "Small Mouth Bass", "Rock Bass", "Pickerel", "Perch", "Sunny", "White Perch", "Crappie", "Trout", "White Bass", ]
+      image: "",
+      coordinates: [],
+      latitude: "",
+      longitude: "",
+      reservoirs: ["Muscoot", "Croton"],
+      species: [
+        "Large Mouth Bass",
+        "Small Mouth Bass",
+        "Rock Bass",
+        "Pickerel",
+        "Perch",
+        "Sunny",
+        "White Perch",
+        "Crappie",
+        "Trout",
+        "White Bass",
+      ],
     };
-
   },
   methods: {
+    selectFile(file) {
+      this.image = file[0];
+    },
+    getExif() {
+      var image = this.image;
+
+      EXIF.getData(image, function () {
+        var imageData = this;
+
+        // get latitude from exif data and calculate latitude decimal
+        var latDegree = imageData.exifdata.GPSLatitude[0].numerator;
+        var latMinute = imageData.exifdata.GPSLatitude[1].numerator;
+        var latSecond = imageData.exifdata.GPSLatitude[2].numerator;
+        var latDirection = imageData.exifdata.GPSLatitudeRef;
+
+        var latFinal = convertDMSToDD(
+          latDegree,
+          latMinute,
+          latSecond,
+          latDirection
+        );
+        //console.log(latFinal);
+
+        // get longitude from exif data and calculate longitude decimal
+        var lonDegree = imageData.exifdata.GPSLongitude[0].numerator;
+        var lonMinute = imageData.exifdata.GPSLongitude[1].numerator;
+        var lonSecond = imageData.exifdata.GPSLongitude[2].numerator;
+        var lonDirection = imageData.exifdata.GPSLongitudeRef;
+
+        var lonFinal = convertDMSToDD(
+          lonDegree,
+          lonMinute,
+          lonSecond,
+          lonDirection
+        );
+        console.log(lonFinal);
+        return latFinal;
+      });
+      function convertDMSToDD(degrees, minutes, seconds, direction) {
+        var dd = degrees + minutes / 60 + seconds / 360000;
+        if (direction == "S" || direction == "W") {
+          dd = dd * -1;
+        }
+        return dd;
+      }
+    },
 
     async submitForm() {
       const formData = new FormData();
+      formData.append("image", this.image);
       formData.append("species", this.record.species);
       formData.append("angler", this.record.angler);
       formData.append("weight", this.record.weight);
       formData.append("reservoir", this.record.reservoir);
       formData.append("comment", this.record.comment);
+      formData.append("latitude", this.latitude);
+      formData.append("longitude", this.longitude);
       if (this.$refs.form.validate()) {
         const response = await API.addRecord(formData);
         this.$router.push({
@@ -84,7 +170,6 @@ export default {
           params: { message: response.message },
         });
       }
-        
     },
   },
 };
