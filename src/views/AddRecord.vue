06335e0b7625a25cdcbef48a023f03e83012246e
Kevin Schoen <kevinschoen05@gmail.com>
2022-03-17 19:46:54 -0400

    removed picture upload and replaced w/ getlocation

--- src/views/AddRecord.vue
+++ src/views/AddRecord.vue
@@ -1,5 +1,13 @@
 <template>
   <v-container>
+    <v-alert
+      border="left"
+      close-text="Close Alert"
+      color="green accent-4"
+      dismissible
+      v-if="this.locationAdded"
+      >{{ "Location Successfully Added" }}</v-alert
+    >
     <v-row no-gutters>
       <v-col sm="10" class="mx-auto">
         <v-card class="pa-5">
@@ -42,14 +50,9 @@
               v-model="record.comment"
               prepend-icon="mdi-note"
             ></v-textarea>
-            <v-file-input
-              @change="selectFile"
-              show-size
-              counter
-              multiple
-              label="Select Image to Upload Catch Location"
+            <v-btn class="mt-3 mr-3" @click="getCurrentLocation()"
+              >Add Current Location</v-btn
             >
-            </v-file-input>
             <v-btn type="submit" class="mt-3" color="primary">Add Fish</v-btn>
           </v-form>
         </v-card>
@@ -66,6 +69,9 @@ import API from "../api";
 import exifr from "exifr";
 export default {
   watch: {
+    latitude: function () {
+      this.locationAdded = true;
+    },
     image: function () {
       console.log("Getting coordinate Data");
       this.getExif(this.image);
@@ -74,6 +80,7 @@ export default {
   data() {
     return {
       rules: [(value) => !!value || "This field is required"],
+      locationAdded: false,
       record: {
         species: "",
         angler: "",
@@ -117,6 +124,13 @@ export default {
             (this.longitude = coords.longitude)
           )
         );
+    },
+    getCurrentLocation() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+      console.log(this.latitude, this.longitude);
     },
     async submitForm() {
       const formData = new FormData();
