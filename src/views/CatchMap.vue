47e9284dc8387be519da33008c14797582aa0539
Kevin Schoen <kevinschoen05@gmail.com>
2022-03-17 20:56:31 -0400

    added color coding to gps pins on catch map

--- src/views/CatchMap.vue
+++ src/views/CatchMap.vue
@@ -9,20 +9,72 @@
       solo
       class="search"
     ></v-select>
-    
-      <GmapMap
-        :center="center"
-        :zoom="13.5"
-        style="width: 100%; height: 400px; justify: center"
-        :mapTypeId="mapTypeId"
-      >
-        <gmap-marker
-          :key="index"
-          v-for="(m, index) in markers"
-          :position="m.position"
-          :title="m.title"
-        ></gmap-marker>
-      </GmapMap> 
+
+    <GmapMap
+      :center="center"
+      :zoom="13.5"
+      style="width: 100%; height: 400px; justify: center"
+      :mapTypeId="mapTypeId"
+    >
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :title="m.title"
+        :icon="m.icon"
+      ></gmap-marker>
+    </GmapMap>
+      <ul class="markerKeys">
+        <li class="markerKey-icon">
+          <img
+            src="http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+            alt=""
+          />
+          Large Mouth Bass
+        </li>
+        <li class="markerKey-icon">
+          <img
+            src="http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+            alt=""
+          />
+          Small Mouth Bass
+        </li>
+        <li class="markerKey-icon">
+          <img
+            src="http://maps.google.com/mapfiles/ms/icons/ltblue-dot.png"
+            alt=""
+          />
+          Rock Bass
+        </li>
+        <li class="markerKey-icon">
+          <img
+            src="http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+            alt=""
+          />
+          Pickerel
+        </li>
+        <li class="markerKey-icon">
+          <img
+            src="http://maps.google.com/mapfiles/ms/icons/orange-dot.png"
+            alt=""
+          />
+          Perch
+        </li>
+        <li class="markerKey-icon">
+          <img
+            src="http://maps.google.com/mapfiles/ms/icons/pink-dot.png"
+            alt=""
+          />
+          Sunny
+        </li>
+        <li class="markerKey-icon">
+          <img
+            src="http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
+            alt=""
+          />
+          Crappie
+        </li>
+      </ul>
   </v-container>
 </template>
 
@@ -75,11 +127,52 @@ export default {
         var lng = parseFloat(recordsWithCoords[i].longitude);
         var label =
           recordsWithCoords[i].species + "\n" + recordsWithCoords[i].angler;
+        var url = this.getMarkerColor(recordsWithCoords[i].species);
+        console.log(url);
         var marker = { lat, lng };
         console.log(marker);
-        this.markers.push({ position: marker, title: label });
+        this.markers.push({ position: marker, title: label, icon: url });
         console.log(this.markers);
       }
+    },
+
+    getMarkerColor(fishSpecies) {
+      switch (fishSpecies) {
+        case "Large Mouth Bass":
+          var url = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
+          break;
+        case "Small Mouth Bass":
+          url = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
+          break;
+        case "Rock Bass":
+          url = "http://maps.google.com/mapfiles/ms/icons/ltblue-dot.png";
+          break;
+        case "Pickerel":
+          url = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+          break;
+        case "Perch":
+          url = "http://maps.google.com/mapfiles/ms/icons/orange-dot.png";
+          break;
+        case "Sunny":
+          url = "http://maps.google.com/mapfiles/ms/icons/pink-dot.png";
+          break;
+        case "White Perch":
+          url = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+          break;
+        case "Crappie":
+          url = "http://maps.google.com/mapfiles/ms/icons/purple-dot.png";
+          break;
+        case "Trout":
+          url = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+          break;
+        case "White Bass":
+          url = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+          break;
+        case "Carp":
+          url = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+          break;
+      }
+      return url;
     },
   },
   async created() {
@@ -95,5 +188,12 @@ export default {
   display: flex;
   width: 80%;
   justify-content: center;
+}
+.markerKeys {
+  display: inline;
+  margin-top: 20px;
+}
+.markerKey-icon {
+  display: inline;
 }
 </style>>
